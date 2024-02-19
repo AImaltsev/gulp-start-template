a7c6fbd04abf0ce4c8f56c3db9001aa15a7ff2c1
AImaltsev <boomer47.am@gmail.com>
2024-02-19 22:13:16 +0400

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+const { src, dest, parallel } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const pug = require('gulp-pug');
+const browserSync = require('browser-sync').create();
+
+//функция запуска сервера
+const browserSyncJob = () => {
+  browserSync.init({
+    server: "build/"
+  });
+
+  watch('src/sass/*.scss', buildSass); //Отслеживание изменений в SASS
+  watch('src/pages/*.pug', buildPug); //Отслеживание изменений в pug
+};
+
+//Компилятор SASS
+const buildSass = () => {
+  console.log('Компиляция SASS');
+
+  return src('src/sass/*.scss')
+    .pipe(sass())
+    .pipe(dest('build/styles/'))
+    .pipe(browserSync.stream());
+}
+
+//Компилятор PUG
+const buildPug = () => {
+  console.log('Компиляция Pug');
+
+  return src('src/pages/*.pug')
+    .pipe(pug())
+    .pipe(dest('build/'))
+    .pipe(browserSync.stream());
+}
+
+exports.server = browserSyncJob; //Сервер
+exports.build = parallel(buildPug, buildSass); //Компиляторы
