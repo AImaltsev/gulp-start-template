361f33aef519ef9713cec3e6f815101bc09cc53e
AImaltsev <boomer47.am@gmail.com>
2024-02-20 23:21:51 +0400

    add image-min

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,22 @@ const { src, dest, parallel, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const pug = require('gulp-pug');
 const browserSync = require('browser-sync').create();
+const imagemin = require('gulp-imagemin');
+const watchFiles = require('gulp-watch');
 
 //функция запуска сервера
 const browserSyncJob = () => {
+  buildSass();
+  buildPug();
+  optimizeImages();
+
   browserSync.init({
     server: "build/"
   });
 
   watch('src/sass/*.scss', buildSass); //Отслеживание изменений в SASS
   watch('src/pages/*.pug', buildPug); //Отслеживание изменений в pug
+  watch('src/images/**/*', optimizeImages); // Отслеживаем изменения в изображениях
 };
 
 //Компилятор SASS
@@ -36,5 +43,17 @@ const buildPug = () => {
     .pipe(browserSync.stream());
 }
 
+// Оптимизация изображений
+const optimizeImages = () => {
+  return src('src/images/**/*')
+    .pipe(imagemin())
+    .pipe(dest('build/images/', { base: 'src/images/' }));
+}
+
+// Отслеживание изменений в изображениях
+const watchImages = () => {
+  watchFiles('src/images/**/*', optimizeImages);
+}
+
 exports.server = browserSyncJob; //Сервер
-exports.build = parallel(buildPug, buildSass); //Компиляторы
+exports.build = parallel(buildPug, buildSass, optimizeImages); //Компиляторы
