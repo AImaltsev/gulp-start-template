5307e5cb0fa17db73bf02318d8c17eaed4f41e30
AImaltsev <boomer47.am@gmail.com>
2024-02-21 09:52:08 +0400

    edit igame-min, add normalize.css

--- gulpfile.js
+++ gulpfile.js
@@ -2,35 +2,36 @@ const { src, dest, parallel, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const pug = require('gulp-pug');
 const browserSync = require('browser-sync').create();
-const imagemin = require('gulp-imagemin');
 const watchFiles = require('gulp-watch');
+const concat = require('gulp-concat');
 
-//функция запуска сервера
-const browserSyncJob = () => {
+// Функция запуска сервера
+const browserSyncJob = async () => {
   buildSass();
   buildPug();
-  optimizeImages();
+  await optimizeImages(); // Ждем завершения оптимизации изображений
 
   browserSync.init({
     server: "build/"
   });
 
-  watch('src/sass/*.scss', buildSass); //Отслеживание изменений в SASS
-  watch('src/pages/*.pug', buildPug); //Отслеживание изменений в pug
+  watch('src/sass/*.scss', buildSass); // Отслеживание изменений в SASS
+  watch('src/pages/*.pug', buildPug); // Отслеживание изменений в Pug
   watch('src/images/**/*', optimizeImages); // Отслеживаем изменения в изображениях
 };
 
-//Компилятор SASS
+// Компилятор SASS
 const buildSass = () => {
   console.log('Компиляция SASS');
 
-  return src('src/sass/*.scss')
+  return src(['node_modules/normalize.css/normalize.css', 'src/sass/*.scss'])
+    .pipe(concat('app.css'))
     .pipe(sass())
     .pipe(dest('build/styles/'))
     .pipe(browserSync.stream());
-}
+};
 
-//Компилятор PUG
+// Компилятор PUG
 const buildPug = () => {
   console.log('Компиляция Pug');
 
@@ -41,19 +42,21 @@ const buildPug = () => {
     }))
     .pipe(dest('build/'))
     .pipe(browserSync.stream());
-}
+};
 
 // Оптимизация изображений
-const optimizeImages = () => {
+const optimizeImages = async () => {
+  const imagemin = await import('gulp-imagemin');
+
   return src('src/images/**/*')
-    .pipe(imagemin())
+    .pipe(imagemin.default()) // Не забудьте использовать .default() для загруженного модуля
     .pipe(dest('build/images/', { base: 'src/images/' }));
-}
+};
 
 // Отслеживание изменений в изображениях
 const watchImages = () => {
   watchFiles('src/images/**/*', optimizeImages);
-}
+};
 
-exports.server = browserSyncJob; //Сервер
-exports.build = parallel(buildPug, buildSass, optimizeImages); //Компиляторы
+exports.server = browserSyncJob; // Сервер
+exports.build = parallel(buildPug, buildSass, optimizeImages); // Компиляторы
